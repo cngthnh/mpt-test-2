05f0c37101c75a6988c2a6fdf4e291bb0319902b
Cong Thanh Vu <63834773+cngthnh@users.noreply.github.com>
2023-09-18 01:21:12 +0700

    Update run-container.sh

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -35,7 +35,7 @@ nohup docker logs -f $container_id > ~/logs/$APP_NAME/$container_id-$(date +%s).
 echo "Waiting for MTurk preview URL: ";
 
 timeout 1800 \
-    sh -c "while ! grep '$M_TURK_PREVIEW_URL_PREFIX' ~/logs/$APP_NAME/$container_id-$(date +%s).log; \
+    sh -c "while ! grep "https://www.mturk.com/mturk/preview?groupId" ~/logs/$APP_NAME/$container_id-$(date +%s).log; \
             do echo "MTurk URL not found"; sleep 1; done";
 
 
