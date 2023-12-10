cec12f43562eb91d86e13cac2a3dcb26175fe151
Cong Thanh Vu <63834773+cngthnh@users.noreply.github.com>
2023-12-10 17:26:36 +0700

    Update deploy.py

--- app/deploy.py
+++ app/deploy.py
@@ -26,7 +26,7 @@ if env == "prod":
     # default_config_file = "prod.yaml"
     default_config_file = "prod_prolific.yaml"
 elif env == "test" or env == "sb":
-    default_config_file = "test.yaml"
+    default_config_file = "test_prolific.yaml"
 
 
 def my_screening_unit_generator():
